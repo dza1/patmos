ab67994a51ab1937df7348116fbe6bc6279a0f64
4tii <david.reitgruber@gmail.com>
2021-04-20 15:46:31 +0200

    bouncing ball maybe

--- c/VGA.c
+++ c/VGA.c
@@ -21,7 +21,12 @@
 #define DISPLAY_HEIGTH 480
 
 #define RAD 100     // radius in pixel
-#define RADCOLOUR 48|0b10<<6 // color of radius
+#define RADCOLOR 48|0b10<<6 // color of radius
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#define BACKGROUND 0
 
 volatile uint8_t (*vga_arr)[DISPLAY_HEIGTH][LINE_WIDTH] = 400000;
 void drawCircle(int m_x, int m_y, int r,uint8_t color);
@@ -46,8 +51,19 @@ int main() {
     }
 
 
-drawCircle( (LINE_WIDTH/2), (DISPLAY_HEIGTH/2), RAD,RADCOLOUR);
 
+
+drawCircle(RAD, (DISPLAY_HEIGTH/2), RAD, RADCOLOR);
+int m_x_new, m_x_old=RAD;
+int m_y_new, m_y_old=DISPLAY_HEIGTH/2;
+
+for(int i=RAD; i<=LINE_WIDTH-RAD; i++){
+  m_x_new = (m_x_old + 1)%(LINE_WIDTH-2*RAD)+RAD;
+  m_y_new = (m_y_old + 1)%(DISPLAY_HEIGTH-2*RAD)+RAD;
+  drawChanges(m_x_old, m_y_old, m_x_new, m_y_new, RAD, RADCOLOR);
+  m_x_old=m_x_new;
+  m_y_old=m_y_new;
+}
 
   int i, j;
   for (;;) {
@@ -79,5 +95,38 @@ void drawCircle(int m_x, int m_y, int r,uint8_t color){
       (*vga_arr)[y][x] = color;
       }
     }
+  }
+}
+
+
+void drawChanges(int m_x_old, int m_y_old, int m_x_new, int m_y_new, int r, uint8_t color){
+  for(int y = MIN(m_y_old, m_y_new)-r; y>=MAX(m_y_old, m_y_new)+r; y++){
+    int* x_start_old;
+    int* x_start_new;
+    int* x_stop_old;
+    int* x_stop_new;
+
+    calcStartStop(m_x_old, m_y_old, y, r, x_start_old, x_stop_old);
+    calcStartStop(m_x_new, m_y_new, y, r, x_start_new, x_stop_new);
+
+    for(int x = MIN(m_x_old, m_x_new)-r; x>=MAX(m_x_old, m_x_new)+r; x++){
+      if(x>=x_start_old && x<x_stop_old && !(x>=x_start_new && x<x_stop_new)){
+        (*vga_arr)[y][x] = BACKGROUND;
+      }else if(x>=x_start_new && x<x_stop_new){
+        (*vga_arr)[y][x] = color;
+      }
+    }
+  }
+}
+
+
+void calcStartStop(int m_x, int m_y, int y, int r, int* x_start, int* x_stop){
+  if((m_y-r)<=y && (m_y+r)>=y){
+    int y_rel=y-m_y;
+    *x_start=m_x-sqrt(r*r-y_rel*y_rel);
+    *x_stop=m_x+sqrt(r*r-y_rel*y_rel);
+  }else{
+    *x_start=0;
+    *x_stop=0;
   }
 }
